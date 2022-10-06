468bb38dff457bea3bb705dbcf04fa034d5273f6
Ricardo Sobjak <ricardosobjak@gmail.com>
2022-10-06 15:55:09 -0300

    angular

---
+++ javascript/angular-aula/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-meu-primeiro',
+    template: `
+        <hr>
+        <h2>Primeiro componente</h2>
+    `,
+    styles: [ 'h2 {color: red} ' ]
+})
+export class MeuPrimeiroComponent {
+    
+}
